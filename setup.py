3028adeb45bd83f6899b57f6700af8c0237150fe
iburadempa <ibu@radempa.de>
2015-11-16 19:39:33 +0100

    add setup.py to allow installing with pip3 install -e

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jts_erd',
+      version='0.0.1',
+      description=('Entity-relationship diagram for a schema given as '
+                   'JSON-table-schema'),
+      url='http://github.com/iburadempa/jts_erd',
+      author='ibu radempa',
+      author_email='ibu@radempa.de',
+      license='MIT',
+      packages=['jts_erd'],
+      zip_safe=False)
